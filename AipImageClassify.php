b7544f3165fb3f54e81e5a361b8a8509af3f3721
yuyansong <yuyansong@baidu.com>
2017-09-08 15:11:06 +0800

    php sdk 1.6.5

---
+++ AipImageClassify.php
@@ -0,0 +1,201 @@
+<?php
+/*
+* Copyright (c) 2017 Baidu.com, Inc. All Rights Reserved
+*
+* Licensed under the Apache License, Version 2.0 (the "License"); you may not
+* use this file except in compliance with the License. You may obtain a copy of
+* the License at
+*
+* Http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+* WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+* License for the specific language governing permissions and limitations under
+* the License.
+*/
+
+require_once 'lib/AipBase.php';
+
+/**
+ * 图像识别
+ */
+class AipImageClassify extends AipBase{
+
+    /**
+     * 菜品识别 dish_detect api url
+     * @var string
+     */
+    private $dishDetectUrl = 'https://aip.baidubce.com/rest/2.0/image-classify/v2/dish';
+
+    /**
+     * 车辆识别 car_detect api url
+     * @var string
+     */
+    private $carDetectUrl = 'https://aip.baidubce.com/rest/2.0/image-classify/v1/car';
+
+    /**
+     * logo商标识别 logo_search api url
+     * @var string
+     */
+    private $logoSearchUrl = 'https://aip.baidubce.com/rest/2.0/image-classify/v2/logo';
+
+    /**
+     * logo入库 logo_add api url
+     * @var string
+     */
+    private $logoAddUrl = 'https://aip.baidubce.com/rest/2.0/realtime_search/v1/logo/add';
+
+    /**
+     * 删除logo logo_delete api url
+     * @var string
+     */
+    private $logoDeleteUrl = 'https://aip.baidubce.com/rest/2.0/realtime_search/v1/logo/delete';
+
+    /**
+     * 图像主体检测 object_detect api url
+     * @var string
+     */
+    private $objectDetectUrl = 'https://aip.baidubce.com/rest/2.0/image-classify/v1/object_detect';
+
+    
+    /**
+     * 菜品识别接口
+     * 该请求用于菜品识别。即对于输入的一张图片（可正常解码，且长宽比适宜），输出图片的菜品名称、卡路里信息、置信度。
+     *
+     * @param string $image - 图像数据，base64编码，要求base64编码后大小不超过4M，最短边至少15px，最长边最大4096px,支持jpg/png/bmp格式
+     * @param array $options - 可选参数对象，key: value都为string类型
+     * @description options列表:
+     *   top_num 返回预测得分top结果数，默认为5
+     * @return array
+     */
+    public function dishDetect($image, $options=array()) {
+        $data = array();
+        
+        $data['image'] = base64_encode($image);
+
+        $data = array_merge($data, $options);
+
+        return $this->request($this->dishDetectUrl, $data);
+    }
+    
+    /**
+     * 车辆识别接口
+     * 该请求用于检测一张车辆图片的具体车型。即对于输入的一张图片（可正常解码，且长宽比适宜），输出图片的车辆品牌及型号。
+     *
+     * @param string $image - 图像数据，base64编码，要求base64编码后大小不超过4M，最短边至少15px，最长边最大4096px,支持jpg/png/bmp格式
+     * @param array $options - 可选参数对象，key: value都为string类型
+     * @description options列表:
+     *   top_num 返回预测得分top结果数，默认为5
+     * @return array
+     */
+    public function carDetect($image, $options=array()) {
+        $data = array();
+        
+        $data['image'] = base64_encode($image);
+
+        $data = array_merge($data, $options);
+
+        return $this->request($this->carDetectUrl, $data);
+    }
+    
+    /**
+     * logo商标识别接口
+     * 该请求用于检测和识别图片中的品牌LOGO信息。即对于输入的一张图片（可正常解码，且长宽比适宜）， 输出图片中LOGO的名称、位置和置信度。logo商标识别支持子库功能，包含入库、检索、删除三个子接口； 其中logo商标识别—查找子接口支持在logo底库（包含2万+logo）中进行查找，可直接使用。 当效果欠佳时，可以建立子库（请加入QQ群：649285136 联系工作人员完成建库）并自定义logo入库，提高识别效果。
+     *
+     * @param string $image - 图像数据，base64编码，要求base64编码后大小不超过4M，最短边至少15px，最长边最大4096px,支持jpg/png/bmp格式
+     * @param array $options - 可选参数对象，key: value都为string类型
+     * @description options列表:
+     *   custom_lib 是否只使用自定义logo库的结果，默认false：返回自定义库+默认库的识别结果
+     * @return array
+     */
+    public function logoSearch($image, $options=array()) {
+        $data = array();
+        
+        $data['image'] = base64_encode($image);
+
+        $data = array_merge($data, $options);
+
+        return $this->request($this->logoSearchUrl, $data);
+    }
+    
+    /**
+     * logo入库接口
+     * 输入一张图片（可正常解码，且长宽比适宜），输出识别出的logo位置、名称和置信度。
+     *
+     * @param string $image - 图像数据，base64编码，要求base64编码后大小不超过4M，最短边至少15px，最长边最大4096px,支持jpg/png/bmp格式*
+     * @param string $brief - brief，检索时带回。此处要传对应的name与code字段，name长度小于100B，code长度小于150B
+     * @param array $options - 可选参数对象，key: value都为string类型
+     * @description options列表:
+     * @return array
+     */
+    public function logoAdd($image, $brief, $options=array()) {
+        $data = array();
+        
+        $data['image'] = base64_encode($image);
+        $data['brief'] = $brief;
+
+        $data = array_merge($data, $options);
+
+        return $this->request($this->logoAddUrl, $data);
+    }
+    
+    /**
+     * 删除logo接口
+     * 删除logo
+     *
+     * @param string $image - 图像数据，base64编码，要求base64编码后大小不超过4M，最短边至少15px，最长边最大4096px,支持jpg/png/bmp格式
+     * @param array $options - 可选参数对象，key: value都为string类型
+     * @description options列表:
+     * @return array
+     */
+    public function logoDeleteByImage($image, $options=array()) {
+        $data = array();
+        
+        $data['image'] = base64_encode($image);
+
+        $data = array_merge($data, $options);
+
+        return $this->request($this->logoDeleteUrl, $data);
+    }
+    
+    /**
+     * 删除logo接口
+     * 删除logo
+     *
+     * @param string $contSign - 图片签名（和image二选一，image优先级更高）
+     * @param array $options - 可选参数对象，key: value都为string类型
+     * @description options列表:
+     * @return array
+     */
+    public function logoDeleteBySign($contSign, $options=array()) {
+        $data = array();
+        
+        $data['cont_sign'] = $cont_sign;
+
+        $data = array_merge($data, $options);
+
+        return $this->request($this->logoDeleteUrl, $data);
+    }
+    
+    /**
+     * 图像主体检测接口
+     * 用户向服务请求检测图像中的主体位置。
+     *
+     * @param string $image - 图像数据，base64编码，要求base64编码后大小不超过4M，最短边至少15px，最长边最大4096px,支持jpg/png/bmp格式
+     * @param array $options - 可选参数对象，key: value都为string类型
+     * @description options列表:
+     *   with_face 如果检测主体是人，主体区域是否带上人脸部分，0-不带人脸区域，其他-带人脸区域，裁剪类需求推荐带人脸，检索/识别类需求推荐不带人脸。默认取1，带人脸。
+     * @return array
+     */
+    public function objectDetect($image, $options=array()) {
+        $data = array();
+        
+        $data['image'] = base64_encode($image);
+
+        $data = array_merge($data, $options);
+
+        return $this->request($this->objectDetectUrl, $data);
+    }
+    
+}
